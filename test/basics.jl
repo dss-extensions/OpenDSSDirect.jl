15e2aa5f329b90dd6e17bceb2c2ce7045838534c
Tom Short <tshort.rlists@gmail.com>
2016-02-14 16:24:33 -0500

    Fix #1, remove more Pkg.dir use

--- test/basics.jl
+++ test/basics.jl
@@ -4,20 +4,20 @@ using OpenDSSDirect
 using Base.Test
 
 
-filename = joinpath(Pkg.dir(), "OpenDSSDirect", "examples", "8500-Node", "Master.dss")
+filename = string("$(dirname(@__FILE__))/../examples/8500-Node/Master.dss")
 
 dss("""
     clear
     compile $filename
 """)
-loadnumber = DSS.loads(Val{:First})
-@test DSS.loads(Val{:Name}) == "138236b0"
+loadnumber = DSS.Loads.First()
+@test DSS.Loads.Name() == "138236b0"
 while loadnumber > 0
-    DSS.loads(Val{:kW},50.)
-    DSS.loads(Val{:kvar},20.)
-    loadnumber = DSS.loads(Val{:Next})
+    DSS.Loads.kW(50.)
+    DSS.Loads.kvar(20.)
+    loadnumber = DSS.Loads.Next()
 end
-@test DSS.loads.Count() == 1177
+@test DSS.Loads.Count() == 1177
 
 end # module
 
@@ -26,7 +26,7 @@ module Y
 using OpenDSSDirect.DSS
 using Base.Test
 
-filename = joinpath(Pkg.dir(), "OpenDSSDirect", "examples", "8500-Node", "Master.dss")
+filename = string("$(dirname(@__FILE__))/../examples/8500-Node/Master.dss")
 
 dss("""
     clear
@@ -34,34 +34,34 @@ dss("""
     solve
 """)
 
-loadnumber = loads(Val{:First})
-@test loads(Val{:Name}) == "138236b0"
+loadnumber = Loads.First()
+@test Loads.Name() == "138236b0"
 kWsum = 0.0
 kvarsum = 0.0
 while loadnumber > 0
-    kWsum += loads(Val{:kW})
-    kvarsum += loads(Val{:kvar})
-    loadnumber = loads(Val{:Next})
+    kWsum += Loads.kW()
+    kvarsum += Loads.kvar()
+    loadnumber = Loads.Next()
 end
 
 linelosssum = 0.0im
-linenumber = Lines(Val{:First})
-n = Lines(Val{:Count})
+linenumber = Lines.First()
+n = Lines.Count()
 linelosses = zeros(Complex128, n)
 for i in 1:n
-    linelosssum += CktElement(Val{:Losses})[1]
-    linelosses[i] = CktElement(Val{:Losses})[1]
-    linenumber = Lines(Val{:Next})
+    linelosssum += CktElement.Losses()[1]
+    linelosses[i] = CktElement.Losses()[1]
+    linenumber = Lines.Next()
 end
-@test_approx_eq_eps linelosssum.re circuit.LineLosses().re * 1000  50
+@test_approx_eq_eps linelosssum.re Circuit.LineLosses().re * 1000  50
 
-@test solution.Mode() == 0
+@test Solution.Mode() == 0
 
-circuit.Losses()
-circuit.SubstationLosses()
-circuit.LineLosses()
-circuit.TotalPower()
-circuit.AllElementLosses()
-circuit.AllBusNames()
+Circuit.Losses()
+Circuit.SubstationLosses()
+Circuit.LineLosses()
+Circuit.TotalPower()
+Circuit.AllElementLosses()
+Circuit.AllBusNames()
 
 end # module
