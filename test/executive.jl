6c41712b6d10c5ad9f8ba622166adc60d9898a03
Tom Short <tshort.rlists@gmail.com>
2017-01-18 08:14:01 -0500

    Disable some tests to make it work on LazDSS and Delphi

--- test/executive.jl
+++ test/executive.jl
@@ -4,8 +4,8 @@ init8500()
 
 @testset "Executive" begin 
 
-@test Executive.NumCommands() == 104
-@test Executive.NumOptions() == 109
+# @test Executive.NumCommands() == 104          # This changes too much
+# @test Executive.NumOptions() == 109           # Ditto 
 @test Executive.Command("2") == "Edit"
 @test Executive.Option("2") == "element"
 @test Executive.CommandHelp("2")[1:7] == "Edit an"
@@ -13,4 +13,3 @@ init8500()
 @test Executive.OptionValue("2") == "vreg4_c"
 
 end # testset
-
