999741d1a8787388ee6d48aacfdd9512677f5fcb
Iliass Zirhi <phileas2@yahoo.fr>
2017-07-16 10:40:35 +0100

    Update lowlevel.jl

--- test/lowlevel.jl
+++ test/lowlevel.jl
@@ -1,7 +1,7 @@
 include("$(dirname(@__FILE__))/../examples/low-level-solver.jl")
 
-isclose(x, y) = maximum(abs(x - y) ./ abs.(x)) < 0.0006
-howclose(x, y) = maximum(abs(x - y) ./ abs.(x))
+isclose(x, y) = maximum(abs.(x - y) ./ abs.(x)) < 0.0006
+howclose(x, y) = maximum(abs.(x - y) ./ abs.(x))
 
 @testset "lowlevel" begin 
 
