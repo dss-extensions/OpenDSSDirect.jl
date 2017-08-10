c536168ab0d4a51f21cc45925b9b1bec92fe134b
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2017-08-10 14:08:43 -0600

    Add mac version (#21)
    * Add Mac OS X dylib and klusolve dylib
    
    Squashing all previous commits
    
    - dlopen libklusolve
    - Redo apple dylib locations
    - Rename lib
    - Another directory name fix
    - another try
    - Another try using dylibs from @kdheepak.
    - Try using Max's libraries
    - Remove versioned libs
    - Test with cd
    - Fix
    
    * Update libopendssdirect.dylib with `@loader_path` instead of `@rpath`
    
    * Remove duplicate dylibs
    
    * Update x86 libopendssdirect.dylib with `@loader_path`
    
    * Combine x64 and x86 libopendssdirect.dylib
    
    Do not load klusolve.dylib in core.jl
    Load from `apple` folder instead of `apple/x64`
    Combine x64 and x86 using lipo
    
    * Remove COMProxy_ControlQueue.CSV
    
    * Update lowlevel.jl
    
    * Update low-level-solver.jl
    
    * Update low-level-solver.jl
    
    * Update lowlevel.jl
    
    * Fix whitespace in examples/low-level-solver.jl
    
    * Make normalsolution_alt conditionally run

--- examples/low-level-solver.jl
+++ examples/low-level-solver.jl
@@ -7,7 +7,7 @@ using OpenDSSDirect
 Equivalent to `DSS.Solution.SolveNoControl()` or `ActiveCircuit.Solution.DoNormalSolution` in Pascal.
 """
 function normalsolution()
-    nodeVbase = abs(DSS.Circuit.AllBusVolts()) ./ DSS.Circuit.AllBusMagPu()   # KLUDGE!
+    nodeVbase = abs.(DSS.Circuit.AllBusVolts()) ./ DSS.Circuit.AllBusMagPu()   # KLUDGE!
     WHOLEMATRIX = 2
     NORMALSOLVE = 0
     nodeV = DSSCore.getV()
@@ -19,7 +19,7 @@ function normalsolution()
 
         # The above call could change the primitive Y matrix, so have to check
         if DSSCore.SystemYChanged()
-            DSSCore.BuildYMatrixD(WHOLEMATRIX, false) 
+            DSSCore.BuildYMatrixD(WHOLEMATRIX, false)
         end
 
         if DSSCore.UseAuxCurrents()
@@ -38,7 +38,7 @@ Alternative `normalsolution` using Julia's built-in sparse solver.
 Equivalent to `DSS.Solution.SolveNoControl()` or `ActiveCircuit.Solution.DoNormalSolution` in Pascal.
 """
 function normalsolution_alt()
-    nodeVbase = abs(DSS.Circuit.AllBusVolts()) ./ DSS.Circuit.AllBusMagPu()   # KLUDGE!
+    nodeVbase = abs.(DSS.Circuit.AllBusVolts()) ./ DSS.Circuit.AllBusMagPu()   # KLUDGE!
     WHOLEMATRIX = 2
     NORMALSOLVE = 0
     nodeV = DSSCore.getV()
@@ -52,7 +52,7 @@ function normalsolution_alt()
 
         # The above call could change the primitive Y matrix, so have to check
         if DSSCore.SystemYChanged()
-            DSSCore.BuildYMatrixD(WHOLEMATRIX, false) 
+            DSSCore.BuildYMatrixD(WHOLEMATRIX, false)
             Y = DSSCore.getYsparse()
         end
 
@@ -86,7 +86,7 @@ function solvesnap()
     while true
         ## Increment the ControlIteration:
         i = DSS.Solution.ControlIterations() + 1
-        DSS.Solution.ControlIterations(i) 
+        DSS.Solution.ControlIterations(i)
         result = normalsolution()
         DSS.Solution.CheckControls()
         if DSS.Solution.ControlActionsDone() == 1
@@ -105,17 +105,17 @@ Check for convervence, and set the DSS status variable `Converged`.
 function converged(nodeV, lastVmag, nodeVbase, tolerance = 0.0001)
     maxerr = 0.0
     for i in 2:length(nodeV)
-        Vmag = abs(nodeV[i])
+        Vmag = abs.(nodeV[i])
         if nodeVbase[i-1] > 0.0     # if base specified, use it; otherwise go on present magnitude
-            err = abs(Vmag - lastVmag[i]) / nodeVbase[i-1]
+            err = abs.(Vmag - lastVmag[i]) / nodeVbase[i-1]
         elseif Vmag != 0.0
-            err = abs(1 - lastVmag[i]/Vmag)
+            err = abs.(1 - lastVmag[i]/Vmag)
         else
             err = 0.0
         end
         lastVmag[i] = Vmag
         if err > maxerr
-            maxerr = err 
+            maxerr = err
         end
     end
     DSS.Solution.Converged(maxerr < tolerance)
@@ -137,7 +137,9 @@ function testnormalsolution()
     i = normalsolution()
     v2 = DSS.Circuit.AllBusVMag()
     init8500a()
-    i = normalsolution_alt()
+    if !is_apple()
+        i = normalsolution_alt()
+    end
     v3 = DSS.Circuit.AllBusVMag()
     (v1, v2, v3)
 end
