8dd622dd786938bb4ca9ae93d34cd08472c1cb03
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2019-01-30 13:17:06 -0700

    Make build.jl download dependencies for all platforms

--- src/OpenDSSDirect.jl
+++ src/OpenDSSDirect.jl
@@ -1,8 +1,6 @@
 
 module OpenDSSDirect
 
-import Libdl
-
 export LIBRARY
 
 export ActiveClass
@@ -44,26 +42,29 @@ export Vsources
 export XYCurves
 export YMatrix
 
+import Libdl
+
 const path = @__DIR__
 if Sys.iswindows()
-    const LIBRARY = joinpath(path, "../deps/win64/dss_capi_v7.dll") |> normpath
+    const LIBRARY = joinpath(path, "../deps/windows/dss_capi_v7.dll") |> normpath
 elseif Sys.islinux()
-    const LIBRARY = joinpath(path, "../deps/linux/libdss_capi_v7.so")
+    const LIBRARY = joinpath(path, "../deps/linux/libdss_capi_v7.so") |> normpath
 else
-    const LIBRARY = joinpath(path, "../deps/apple/libdss_capi_v7.dylib")
+    const LIBRARY = joinpath(path, "../deps/apple/libdss_capi_v7.dylib") |> normpath
 end
 
 function __init__()
 
+
     if Sys.iswindows()
-        KLUSOLVE_LIBRARY = joinpath(path, "../deps/win64/libklusolve.dll") |> normpath
+        KLUSOLVE_LIBRARY = joinpath(path, "../deps/windows/libklusolve.dll") |> normpath
         if Libdl.dlopen(KLUSOLVE_LIBRARY) == C_NULL
-            error("\$$KLUSOLVE_LIBRARY cannot be opened")
+            error("$KLUSOLVE_LIBRARY cannot be opened. Please check 'deps/build.log' for more information.")
         end
     end
 
     if Libdl.dlopen(LIBRARY) == C_NULL
-        error("\$$LIBRARY cannot be opened")
+        error("$LIBRARY cannot be opened. Please check 'deps/build.log' for more information.")
     end
 
 end
