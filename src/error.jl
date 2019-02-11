7170a157ed335404efba2d081bef203729c41a0d
Paulo Meira <10246101+PMeira@users.noreply.github.com>
2019-02-11 02:04:02 -0200

    Tiny fix for the Error module

--- src/error.jl
+++ src/error.jl
@@ -12,12 +12,12 @@ module Error
 
     """Description of error for last operation"""
     function Description()::String
-        return Utils.get_string(lib.Error_Get_Description())
+        return Utils.get_string(Lib.Error_Get_Description())
     end
 
     """Error Number"""
     function Number()::Int
-        return lib.Error_Get_Number()
+        return Lib.Error_Get_Number()
     end
 
 end
