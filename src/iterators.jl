65cfc5f1ee6b691bc89acee481b5f58f1066b15e
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2019-02-17 14:34:42 -0700

    Add docstring

--- src/iterators.jl
+++ src/iterators.jl
@@ -1,4 +1,38 @@
 
+"""
+    EachMember(m::Module)
+    EachMember(m::Module, f::Function)
+
+Call function `f` from module `m` for each member or each element in module in a iterator fashion.
+
+```julia
+julia> for n in OpenDSSDirect.EachMember(Lines, Lines.Name)
+            @show n
+       end
+n = "hvmv_sub_connector"
+n = "ln5502549-1"
+n = "ln6259988-1"
+n = "ln6077796-1"
+n = "ln5835135-2"
+...
+```
+
+This also works without a function. By default, it returns the index of the elment.
+
+```julia
+julia> for i in OpenDSSDirect.EachMember(Lines)
+           @show i, Lines.Name()
+       end
+(i, Lines.Name()) = (1, "hvmv_sub_connector")
+(i, Lines.Name()) = (2, "ln5502549-1")
+(i, Lines.Name()) = (3, "ln6259988-1")
+(i, Lines.Name()) = (4, "ln6077796-1")
+(i, Lines.Name()) = (5, "ln5835135-2")
+...
+```
+
+In each iteration of the body of the `for` loop a new element of the module can be accessed.
+"""
 struct EachMember
     m::Module
     f::Union{Nothing, Function}
