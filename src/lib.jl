6c0704c699efe34f9c4db6a1386940244ea68299
Dheepak Krishnamurthy <me@kdheepak.com>
2019-12-06 12:51:00 -0700

    Add docstrings for enum

--- src/lib.jl
+++ src/lib.jl
@@ -119,6 +119,60 @@ Base.convert(::Type{LoadStatus}, x::Integer) = LoadStatus(x)
 Base.convert(::Type{LoadModels}, x::Integer) = LoadModels(x)
 Base.convert(::Type{LineUnits}, x::Integer) = LineUnits(x)
 
+@doc """
+$( join(map(x -> "- `" * String(x[1]) * "`: " * string(x[2]), CEnum.name_value_pairs(Lib.MonitorModes)), "\n") )
+"""
+Lib.MonitorModes
+@doc """
+$( join(map(x -> "- `" * String(x[1]) * "`: " * string(x[2]), CEnum.name_value_pairs(Lib.SolveModes)), "\n") )
+"""
+Lib.SolveModes
+@doc """
+$( join(map(x -> "- `" * String(x[1]) * "`: " * string(x[2]), CEnum.name_value_pairs(Lib.SolutionLoadModels)), "\n") )
+"""
+Lib.SolutionLoadModels
+@doc """
+$( join(map(x -> "- `" * String(x[1]) * "`: " * string(x[2]), CEnum.name_value_pairs(Lib.SolutionAlgorithms)), "\n") )
+"""
+Lib.SolutionAlgorithms
+@doc """
+$( join(map(x -> "- `" * String(x[1]) * "`: " * string(x[2]), CEnum.name_value_pairs(Lib.ControlModes)), "\n") )
+"""
+Lib.ControlModes
+@doc """
+$( join(map(x -> "- `" * String(x[1]) * "`: " * string(x[2]), CEnum.name_value_pairs(Lib.CktModels)), "\n") )
+"""
+Lib.CktModels
+@doc """
+$( join(map(x -> "- `" * String(x[1]) * "`: " * string(x[2]), CEnum.name_value_pairs(Lib.RandomModes)), "\n") )
+"""
+Lib.RandomModes
+@doc """
+$( join(map(x -> "- `" * String(x[1]) * "`: " * string(x[2]), CEnum.name_value_pairs(Lib.AutoAddTypes)), "\n") )
+"""
+Lib.AutoAddTypes
+@doc """
+$( join(map(x -> "- `" * String(x[1]) * "`: " * string(x[2]), CEnum.name_value_pairs(Lib.CapControlModes)), "\n") )
+"""
+Lib.CapControlModes
+@doc """
+$( join(map(x -> "- `" * String(x[1]) * "`: " * string(x[2]), CEnum.name_value_pairs(Lib.ActionCodes)), "\n") )
+"""
+Lib.ActionCodes
+@doc """
+$( join(map(x -> "- `" * String(x[1]) * "`: " * string(x[2]), CEnum.name_value_pairs(Lib.LoadStatus)), "\n") )
+"""
+Lib.LoadStatus
+@doc """
+$( join(map(x -> "- `" * String(x[1]) * "`: " * string(x[2]), CEnum.name_value_pairs(Lib.LoadModels)), "\n") )
+"""
+Lib.LoadModels
+@doc """
+$( join(map(x -> "- `" * String(x[1]) * "`: " * string(x[2]), CEnum.name_value_pairs(Lib.LineUnits)), "\n") )
+"""
+Lib.LineUnits
+
+
 function DSS_ResetStringBuffer()
     ccall((:DSS_ResetStringBuffer, LIBRARY), Cvoid, ())
 end
