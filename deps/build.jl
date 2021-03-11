f5f20c6c5b18f6f169276298cf25a242ca0fa5c7
Paulo Meira <10246101+PMeira@users.noreply.github.com>
2021-03-10 23:42:38 -0300

    Upgrade DSS C-API to 0.10.7-1, which includes a fix to an issue with energy meter reports.

--- deps/build.jl
+++ deps/build.jl
@@ -6,7 +6,7 @@ abstract type Windows <: AbstractOS end
 abstract type MacOS <: BSD end
 abstract type Linux <: BSD end
 
-const DSS_CAPI_TAG = "0.10.7"
+const DSS_CAPI_TAG = "0.10.7-1"
 
 function download(::Type{MacOS})
 
