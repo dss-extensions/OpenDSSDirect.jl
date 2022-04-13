dbcd51ff444c984dfd86dbc7a25be13f30ee01d4
Paulo Meira <10246101+PMeira@users.noreply.github.com>
2022-04-13 02:39:47 -0300

    SwtControls: Use Lib.ActionCodes for Action, NormalState and State.
    This mirrors how other enums are used.
    
    Also add tests. The `Idx()` getter was found to be broken, but it's
    already fixed in DSS C-API v0.12.

--- test/swtcontrols.jl
+++ test/swtcontrols.jl
@@ -1,25 +1,40 @@
 
 init8500()
 
+OpenDSSDirect.Text.Command("""
+New SwtControl.ctrl_LN3693186_sw SwitchedObj=line.LN3693186_sw SwitchedTerm=1 Action=open
+""")
 
 @testset "SwtControls" begin
 
-@show SwtControls.First()
-@show SwtControls.Next()
-@show SwtControls.Action()
-@show SwtControls.Action(SwtControls.Action())
-@show SwtControls.IsLocked()
-@show SwtControls.IsLocked(SwtControls.IsLocked())
-@show SwtControls.SwitchedTerm()
-@show SwtControls.SwitchedTerm(SwtControls.SwitchedTerm())
-@show SwtControls.Count()
-@show SwtControls.Delay()
-@show SwtControls.Delay(SwtControls.Delay())
-@show SwtControls.Name()
-@show SwtControls.Name(SwtControls.Name())
-@show SwtControls.SwitchedObj()
-@show SwtControls.SwitchedObj(SwtControls.SwitchedObj())
-@show SwtControls.AllNames()
+@test SwtControls.First() == 1
+#TODO: @test SwtControls.Idx() == 1
+@test SwtControls.Next() == 0
+@test_throws OpenDSSDirect.OpenDSSDirectException SwtControls.Idx(0)
+@test_throws OpenDSSDirect.OpenDSSDirectException SwtControls.Idx(7)
+#TODO: @test SwtControls.Idx() == 1
+@test_broken OpenDSSDirect.Lib.SwtControls_Get_idx() == 1
+@test SwtControls.Action() == OpenDSSDirect.Lib.ActionCodes_Open
+@test SwtControls.Action(OpenDSSDirect.Lib.ActionCodes_none) == nothing
+@test SwtControls.Action() == OpenDSSDirect.Lib.ActionCodes_Open
+@test SwtControls.Action(OpenDSSDirect.Lib.ActionCodes_Close) == nothing
+@test SwtControls.Action() == OpenDSSDirect.Lib.ActionCodes_Close
+@test SwtControls.NormalState() == OpenDSSDirect.Lib.ActionCodes_Open
+@test SwtControls.State() == OpenDSSDirect.Lib.ActionCodes_Close
+@test SwtControls.IsLocked() == false
+@test SwtControls.IsLocked(false) == nothing
+@test SwtControls.SwitchedTerm() == 1
+@test SwtControls.SwitchedTerm(2) == nothing
+@test SwtControls.SwitchedTerm() == 2
+@test SwtControls.Count() == 1
+@test SwtControls.Delay() == 120.0
+@test SwtControls.Delay(300.0) == nothing
+@test SwtControls.Delay() == 300.0
+@test SwtControls.Name() == "ctrl_ln3693186_sw"
+@test SwtControls.Name(SwtControls.Name()) == nothing 
+@test SwtControls.SwitchedObj() == "line.ln3693186_sw"
+@test SwtControls.SwitchedObj("Line.A8611_48332_sw") == nothing
+@test SwtControls.SwitchedObj() == "line.a8611_48332_sw"
 
 arr = String[]
 for i in OpenDSSDirect.EachMember(SwtControls); push!(arr, SwtControls.Name()); end
